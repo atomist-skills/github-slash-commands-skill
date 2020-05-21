d312abdf8299fd8023cf5940dd4dbf39279bc916
Jim Clark <slimslenderslacks@gmail.com>
2020-05-21 16:35:52 -0700

    Bump git-chatops-skill to 0.1.8 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "git-chatops-skill")
 (def path ".")
-(def version "0.1.7")
-(def tag "0.1.7")
-(def generated-at "2020-05-21T23:24:21Z")
+(def version "0.1.8")
+(def tag "0.1.8")
+(def generated-at "2020-05-21T23:35:52Z")
