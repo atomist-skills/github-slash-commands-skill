097b0a0960cf58cbde7168fbd6d1fa31cd2f954e
Jim Clark <slimslenderslacks@gmail.com>
2020-05-22 14:33:06 -0700

    some notes

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -6,7 +6,8 @@
             [goog.string.format]
             [clojure.data]
             [atomist.cljs-log :as log]
-            [atomist.github])
+            [atomist.github]
+            [cljs.spec.alpha :as s])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn no-null-format [f & args]
@@ -15,43 +16,84 @@
     (catch :default _
       f)))
 
-(defn atomist-command [s]
-  (re-find #"(?m)atomist (\w+)(.*)?" s))
+(defn slack-message [{:keys [command args login repo] :as request}]
+  (-> request
+      (api/channel "git-chatops-skill")
+      (api/simple-message (no-null-format "```run %s %s \n    on %s/%s from %s```"
+                                          command
+                                          args
+                                          (:owner repo)
+                                          (:name repo)
+                                          login))))
 
-(defn push-mode [request {[{:keys [branch] {:keys [owner name]} :repo {:keys [message author sha]} :after}] :Push}]
-  (go
-    (let [[_ command args] (atomist-command message)]
-      (if command
-        (<! (-> request
-                (api/channel "git-chatops-skill") gs
-                (api/simple-message (no-null-format "```run %s %s \n    for branch %s on %s/%s from %s```"
-                                                    command
-                                                    args
-                                                    branch
-                                                    owner
-                                                    name
-                                                    (:login author)))))
-        (<! (-> request
-                (api/channel "git-chatops-skill")
-                (api/simple-message (gstring/format "No command in ```%s```" (or message "missing message"))))))
-      request)))
+(defmulti run :command/command)
+(defmethod run "pr" [{:command/keys [args token repo message]
+                      :pr/keys [branch]}]
+  (go (<! (atomist.github/post-pr
+           {:token token :owner (:owner repo) :repo (:name repo)}
+           "title" "body" branch "base"))))
+(defmethod run "label" [{:command/keys [args token repo message]
+                         :label/keys [number]}]
+  (go {:return "return"}))
 
-(defn comment-mode [request {[{:keys [body]}] :Comment}]
-  (go
-    (let [[_ command args] (atomist-command body)]
-      (if command
-        (assoc request :command command :args args)
-        request))))
-
-(defn custom-middleware [handler]
+(defn run-command [handler]
   (fn [request]
     (go
-      (let [data (-> request :data)]
-        (<! (handler
-             (cond
-               (contains? data :Push) (<! (push-mode request data))
-               (contains? data :Comment) (<! (comment-mode request data))
-               :else request)))))))
+      (assoc request :return (<! (run (:command request)))))))
+
+(defn validate-command-spec [d]
+  (if (not (s/valid? :command/spec d))
+    (str (s/explain :command/spec d))))
+
+(defn validate-command [handler]
+  (fn [request]
+    (go
+      (if-let [failure (validate-command-spec (:command request))]
+        (<! (api/finish request :failure failure))
+        (<! (handler request))))))
+
+(defn add-command [handler]
+  (fn [{:keys [command args token repo number branch message] :as request}]
+    (go
+      (<! (slack-message request))
+      (<! (handler (assoc request
+                     :command (merge
+                               {:command/command command
+                                :command/args args
+                                :command/token token
+                                :command/repo repo
+                                :command/message message}
+                               (if (= "label" command)
+                                 {:label/number number})
+                               (if (= "pr" command)
+                                 {:pr/branch branch}))))))))
+
+(defn atomist-command [keyword s]
+  (re-find (re-pattern (gstring/format "(?m)%s (\w+)(.*)?" keyword)) s))
+
+(defn push-mode [{:keys [keyword]} {[{:keys [branch repo] {:keys [message sha] {:keys [login]} :author} :after}] :Push}]
+  (let [[_ command args] (atomist-command keyword message)]
+    (if command
+      {:command command :args args :repo repo :branch branch :login login :message message}
+      :none)))
+
+(defn comment-mode [{:keys [keyword]} {[{:keys [body issue pullRequest] {:keys [login]} :by}] :Comment}]
+  (let [[_ command args] (atomist-command keyword body)]
+    (if (and command (or issue pullRequest))
+      (merge (or issue pullRequest) {:login login :command command :args args :message body})
+      :none)))
+
+(defn check-push-or-comment [handler]
+  (fn [request]
+    (go
+      (let [data (-> request :data)
+            mode (cond
+                   (contains? data :Push) (push-mode request data)
+                   (contains? data :Comment) (comment-mode request data)
+                   :else :none)]
+        (if (not (= mode :none))
+          (<! (handler (merge request mode)))
+          (<! (api/finish request :success "skipping" :visibility :hidden)))))))
 
 (defn ^:export handler
   [data sendreponse]
@@ -59,7 +101,11 @@
    data
    sendreponse
    (-> (api/finished)
-       (custom-middleware)
+       (run-command)
+       (validate-command)
+       (add-command)
+       (check-push-or-comment)
+       (api/add-skill-config :keyword)
        (api/extract-github-token)
        (api/add-slack-source-to-event)
        (api/log-event)
